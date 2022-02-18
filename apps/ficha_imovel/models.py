b6c7b7f6f53c4e701e16341e6ca61d7ee5f3dd94
Leonardo Meireles <meirelesleonardo.ti@gmail.com>
2022-02-18 00:56:18 -0300

    Criado os primeiros models e relacionamentos #3

--- apps/ficha_imovel/models.py
+++ apps/ficha_imovel/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from apps.instituicao.models import Instituicao
 
-# Create your models here.
+
+class Condominio(models.Model):
+    CondominioId = models.AutoField(primary_key=True)
+    Condominio = models.CharField(max_length=1000, blank=True, null=True)
+    Tipo = models.CharField(max_length=100, blank=True, null=True)
+    Bairro = models.CharField(max_length=100, blank=True, null=True)
+    Endereco = models.CharField(max_length=1000, blank=True, null=True)
+    Localizacao = models.CharField(max_length=1000, blank=True, null=True)
+    NomeDoEdificio = models.CharField(max_length=1000, blank=True, null=True)
+
+    def __str__(self):
+        return self.Condominio
+
+
+class FichaImovel(models.Model):
+    FichaImovelId = models.AutoField(primary_key=True)
+    CodigoUnico = models.CharField(max_length=100, blank=True, null=True)
+    DataCadastro = models.DateTimeField(auto_now_add=True)
+    CantoDePedra = models.BooleanField(default=True)
+    MotivoCantoDePedra = models.CharField(max_length=300, blank=True, null=True)
+
+    
+    DataUltimaModificacao = models.DateTimeField(auto_now=True)
+    InstituicaoFk = models.ForeignKey(Instituicao, on_delete=models.CASCADE)
+    CondominioFk = models.ForeignKey(Condominio, on_delete=models.CASCADE, null=True, blank=True)
+    
+
+    def __str__(self):
+        return self.CodigoUnico
+
--- apps/instituicao/models.py
+++ apps/instituicao/models.py
@@ -14,4 +14,7 @@ class Instituicao(models.Model):
     Dominio = models.CharField(max_length=1000, blank=True, null=True)
     DataCadastro = models.DateTimeField(auto_now_add=True)
     Ativo = models.BooleanField(default=True)
-    ficha_instituicao = models.BooleanField(default=True)
+    ficha_instituicao = models.BooleanField(default=True)    
+
+    def __str__(self):
+        return self.NomeFantasia
