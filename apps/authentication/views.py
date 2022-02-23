372c49f6b0cf8d4c334a99a85fe49c3cfb33bc15
Leonardo Meireles <meirelesleonardo.ti@gmail.com>
2022-02-23 00:38:24 -0300

    Primeira limpeza #6

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -25,9 +25,9 @@ def login_view(request):
                 login(request, user)
                 return redirect("/")
             else:
-                msg = 'Invalid credentials'
+                msg = 'Usuário ou senha incorreto'
         else:
-            msg = 'Error validating the form'
+            msg = 'Erro na validação do formulário'
 
     return render(request, "accounts/login.html", {"form": form, "msg": msg})
 
