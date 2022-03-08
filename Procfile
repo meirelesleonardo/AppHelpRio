7051df784179b35cadca6f41966401df9ac320cd
Leonardo Meireles <meirelesleonardo.ti@gmail.com>
2022-03-07 23:13:23 -0300

    Adcionar arquivos estáticos  - whitenoise

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn core.wsgi --log-file -
+release: python3 manage.py migrate
+web: gunicorn core.wsgi --preload --log-file -
--- core/settings.py
+++ core/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 #STATICFILES_DIRS = 1
 
 # Extra places for collectstatic to find static files.
